99c9c275964cb2c4edda56dbf3cb42f8c1589b3e
Gurunishal <71168422+Gurunishal@users.noreply.github.com>
2022-06-13 20:16:42 +0530

    Update Set operations.py

--- Set operations.py
+++ Set operations.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 E = {0, 2, 4, 6, 8};
 N = {1, 2, 3, 4, 5};
 
@@ -16,10 +10,3 @@ print("Union of E and N is",union)
 print("Intersection of E and N is",intersection)
 print("Difference of E and N is",diff)
 print("Symmetric difference of E and N is",E ^ N)
-
-
-# In[ ]:
-
-
-
-
